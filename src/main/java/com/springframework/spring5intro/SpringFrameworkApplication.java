6da6d4fbeea69cada582cbc2a346b6387c001af0
Naresh <naresh@breezeware.net>
2021-04-27 13:42:39 +0530

    Refactored POM.xml

---
+++ src/main/java/com/springframework/spring5intro/SpringFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.spring5intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkApplication.class, args);
+    }
+}
---
+++ src/test/java/com/springframework/spring5intro/SpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spring5intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
